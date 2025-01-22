9d63383392a7eb2afc0a8a79321fbc4351514f28
Carlos Braun <69319659+CarlosBraun@users.noreply.github.com>
2025-01-22 03:32:01 -0300

    update

--- procesamiento.py
+++ procesamiento.py
@@ -262,6 +262,12 @@ def procesar_partidos(file_path,equipo1,equipo2):
     linea_equipo_visita = next((i for i, linea in enumerate(match_data) if equipo2 in linea ), None)
     sets_local = re.search(r'\d+',match_data[linea_equipo_local]).group()
     sets_visita = re.search(r'\d+',match_data[linea_equipo_visita]).group()
+    if int(sets_local) == 3:
+        ganador_local = 1
+        ganador_visita = 0
+    else:
+        ganador_local = 0
+        ganador_visita = 1
     total_sets = int(sets_local)+int(sets_visita)
     Match_id = re.search(r'\d+',match_data[linea_inicio_data_general-1]).group()
     Match_date = re.search(r'\b\d{2}/\d{2}/\d{4}\b', match_data[linea_inicio_data_general]).group()
@@ -275,8 +281,10 @@ def procesar_partidos(file_path,equipo1,equipo2):
     df_match = agregar_columna_id(pd.DataFrame(match_general_data[1:], columns=match_general_data[0]),match_ID)
     df_match["Duración"] = pd.to_numeric(df_match["Duración"], errors="coerce")  # Convierte valores no válidos a NaN
     df_match["Set"] = pd.to_numeric(df_match["Set"], errors="coerce")  # Convierte valores no válidos a NaN
-    total_duracion = df_match["Duración"].sum()
+    total_duracion_temp = df_match["Duración"].sum()
+    total_duracion = str(total_duracion_temp//0.60).split(".")[0]+"."+str(total_duracion_temp%0.60).split(".")[1]
     score_e1,score_e2 = calcular_score_total(df_match["Score"])
+    score_total = str(score_e1)+"-"+str(score_e2)
     break_line = equipo1+" "+equipo2
     equipo1_linea = next((i for i, linea in enumerate(lineas_contenido) if linea.replace(" Set Pontos Serviço Recepção Ataque BK","").strip() == equipo1), None)
     equipo2_linea = next((i for i, linea in enumerate(lineas_contenido) if linea.replace(" Set Pontos Serviço Recepção Ataque BK","").strip() == equipo2), None)
@@ -292,7 +300,6 @@ def procesar_partidos(file_path,equipo1,equipo2):
     datos_por_set_e1 = procesar_info_por_set(equipo1_data[equipo1_fin+1-equipo1_linea:equipo1_fin-equipo1_linea+total_sets+2])
     datos_por_set_e2 = procesar_info_por_set(equipo2_data[equipo2_fin+1-equipo2_linea:equipo2_fin-equipo2_linea+total_sets+2])
     header_2= ['Set', 'Ser', 'Ata', 'BK', 'Error', 'Tot S', 'Err S', 'Pts S', 'Tot R', 'Err R', 'Pos% R', '(Prf%) R', 'Tot A', 'Err A', 'Blo A', 'Pts A', 'Pts% A', 'Pts BK']
-    print(datos_por_set_e1[0])
     df_data_por_set_e1 = agregar_columna_equipo(agregar_columna_id(pd.DataFrame(datos_por_set_e1[1:],columns=header_2),match_ID),equipo1)
     df_data_por_set_e2 = agregar_columna_equipo(agregar_columna_id(pd.DataFrame(datos_por_set_e2[1:],columns=header_2),match_ID),equipo2)
     cambiar_errOP(df_data_por_set_e1,df_data_por_set_e2)
@@ -301,7 +308,13 @@ def procesar_partidos(file_path,equipo1,equipo2):
     datos_jugadores2 = [preprocesar_datos_jugador(linea,equipo2_data[1]) for linea in equipo2_data[2:equipo2_fin+1-equipo2_linea]]
     datos2 = rellenar_datos(datos_jugadores2,headers_1)
     df1 = agregar_columna_equipo(agregar_columna_id(pd.DataFrame(datos1, columns=headers_1),match_ID),equipo1)
+    sub_df1 = df1["Voto"].copy()
+    sub_df1 = pd.to_numeric(sub_df1, errors='coerce')
+    promedio1 = sub_df1.mean()
     df2 = agregar_columna_equipo(agregar_columna_id(pd.DataFrame(datos2, columns=headers_1),match_ID),equipo2)
+    sub_df2 = df2["Voto"].copy()
+    sub_df2 = pd.to_numeric(sub_df2, errors='coerce')
+    promedio2 = sub_df2.mean()
     # print("-----------------")
     # for i in lineas_contenido[break_linea:]:
     #     print(i)
@@ -315,60 +328,76 @@ def procesar_partidos(file_path,equipo1,equipo2):
     else:
         temp1 = data[11]
         temp2 = data[12]
-    if len(data[6].split(" "))<=8:
-        temp3 = clean_string(data[7])
-    else:
-        temp3 = clean_string3(data[6])
+    # if len(data[6].split(" "))<=8:
+    #     temp3 = clean_string(data[7])
+    # else:
+    #     temp3 = clean_string3(data[6])
     serve1,serve2 = extraer_recepcion_y_palabras(clean_string(temp1))
     ace1 , ace2 = extraer_puntos_y_palabras_previas(clean_string2(clean_string(temp2)))
-    attack_positive = eliminar_items_con_letras([temp3.split(" ")])
-    attack_exclamative = eliminar_items_con_letras([clean_string4(temp2).split(" ")])
-    contra = [data[15].split(" ")]
-    headers = ["Err1", "Blo1", "Pts%s1","Tot1", "Tot2", "Pts%s2", "Blo2", "Err2"]
+    # attack_positive = eliminar_items_con_letras([temp3.split(" ")])
+    # attack_exclamative = eliminar_items_con_letras([clean_string4(temp2).split(" ")])
+    # contra = [data[15].split(" ")]
+    # headers = ["Err1", "Blo1", "Pts%s1","Tot1", "Tot2", "Pts%s2", "Blo2", "Err2"]
     headers2 = ["Equipo","Recepcion","Puntos SO","Servicio","Puntos BP"]
     temp = [[equipo1,extraer_numeros(rece1),extraer_numeros(puntos1),extraer_numeros(serve1),extraer_numeros(ace1)],[equipo2,extraer_numeros(rece2),extraer_numeros(puntos2),extraer_numeros(serve2),extraer_numeros(ace2)]]
-    df_positive = pd.DataFrame(attack_positive,columns=headers)
-    df_exclamative = pd.DataFrame(attack_exclamative,columns=headers)
-    df_contra = pd.DataFrame(contra,columns=headers)
+    # df_positive = pd.DataFrame(attack_positive,columns=headers)
+    # df_exclamative = pd.DataFrame(attack_exclamative,columns=headers)
+    # df_contra = pd.DataFrame(contra,columns=headers)
     df_metricas = agregar_columna_id(pd.DataFrame(temp,columns=headers2),match_ID)
-    headers_3 = ["ID","Equipo1","Equipo2","Numero de Partido","Fecha","Sets Local","Sets Visita","Set Total","Duración","Score Acumulado"]
+    # headers_3 = ["ID","Equipo1","Equipo2","Numero de Partido","Fecha","Sets Local","Sets Visita","Set Total","Duración","Score Acumulado"]
     match_info = [[match_ID,equipo1,equipo2,Match_id,Match_date,sets_local,sets_visita,str(int(sets_local)+int(sets_visita)),total_duracion,str(score_e1)+"-"+str(score_e2)]]
-    df_general = pd.DataFrame(match_info,columns=headers_3)
+    # df_general = pd.DataFrame(match_info,columns=headers_3)
+    final_headers = ["Partido","Fecha","Equipo","Ganador","Local","Sets","Duracion","Resultado","Eva","Tot Puntos","BP Puntos","G-P Puntos","Tot Saque","Err Saque","Pts Saque","Tot Recepcion","Err Recepcion","Pos% Recepcion","(Exc%) Recepcion","Tot Ataque","Err Ataque","BL Ataque","Pts Ataque","Pts% Ataque", "Pts BL","Recepciones por Puntos","Saques por Break Point" ]
+    equipo1_datos = [Match_id,Match_date,equipo1,ganador_local,1,sets_local,total_duracion,score_total,float(promedio1)]
+    equipo1_datos.extend(df1.iloc[-1].to_list()[-16:])
+    equipo1_datos.extend([int(df_metricas["Recepcion"][0])/int(df_metricas["Puntos SO"][0]),int(df_metricas["Servicio"][0])/int(df_metricas["Puntos BP"][0])])
+    equipo2_datos = [Match_id,Match_date,equipo2,ganador_visita,0,sets_visita,total_duracion,score_total,float(promedio2)]
+    equipo2_datos.extend(df2.iloc[-1].to_list()[-16:])
+    equipo2_datos.extend([int(df_metricas["Recepcion"][1])/int(df_metricas["Puntos SO"][1]),int(df_metricas["Servicio"][1])/int(df_metricas["Puntos BP"][1])])
 
-    print("PARTIDO "+equipo1.upper()+" v/s "+ equipo2.upper())
-    print(df_general)
+    datos_df = [equipo1_datos,equipo2_datos]
+    df_final = pd.DataFrame(datos_df,columns=final_headers)
 
-    print(df_match)
-    guardar_datos_en_excel(df_match,"DATOS.xlsx","Sets_General")
-    print("Duración total: "+ str(total_duracion))
-    print("Score Total: "+str(score_e1)+"-"+str(score_e2))
-    print("______________________________________________")
-    print("EQUIPO "+equipo1.upper())
-    print(df1)
-    guardar_datos_en_excel(df1,"DATOS.xlsx","Jugadores")
-    print("______________________________________________")
-    print(df_data_por_set_e1)
-    guardar_datos_en_excel(df_data_por_set_e1,"DATOS.xlsx","Por_Set")
-    print("______________________________________________")
-    print("EQUIPO "+equipo2.upper())
-    print(df2)
-    guardar_datos_en_excel(df2,"DATOS.xlsx","Jugadores")
-    print("______________________________________________")
-    print(df_data_por_set_e2)
-    guardar_datos_en_excel(df_data_por_set_e2,"DATOS.xlsx","Por_Set")
-    print("______________________________________________")
-    print("______________________________________________")
+    print("---------------------")
+    print(df_final)
+    guardar_datos_en_excel(df_final,"DATOS.xlsx","Datos")
+    print("---------------------")
 
-    print(df_metricas)
-    guardar_datos_en_excel(df_metricas,"DATOS.xlsx","Recepción_Servicio")
-    print("-----------------")
-    print("1° ATAQUE RECE(+#)")
-    print(df_positive)
-    print("1° ATAQUE RECE(-!)")
-    print(df_exclamative)
-    print("CONTRA ATAQUE")
-    print(df_contra)
-    print("-----------------")
+    # print("PARTIDO "+equipo1.upper()+" v/s "+ equipo2.upper())
+    # print(df_general)
+
+    # print(df_match)
+    # guardar_datos_en_excel(df_match,"DATOS.xlsx","Sets_General")
+    # print("Duración total: "+ str(total_duracion))
+    # print("Score Total: "+str(score_e1)+"-"+str(score_e2))
+    # print("______________________________________________")
+    # print("EQUIPO "+equipo1.upper())
+
+
+    # guardar_datos_en_excel(df1,"DATOS.xlsx","Jugadores")
+    # print("______________________________________________")
+    # print(df_data_por_set_e1)
+    # guardar_datos_en_excel(df_data_por_set_e1,"DATOS.xlsx","Por_Set")
+    # print("______________________________________________")
+    # print("EQUIPO "+equipo2.upper())
+    # print(df2)
+    # guardar_datos_en_excel(df2,"DATOS.xlsx","Jugadores")
+    # print("______________________________________________")
+    # print(df_data_por_set_e2)
+    # guardar_datos_en_excel(df_data_por_set_e2,"DATOS.xlsx","Por_Set")
+    # print("______________________________________________")
+    # print("______________________________________________")
+
+    # print(df_metricas)
+    # guardar_datos_en_excel(df_metricas,"DATOS.xlsx","Recepción_Servicio")
+    # print("-----------------")
+    # print("1° ATAQUE RECE(+#)")
+    # print(df_positive)
+    # print("1° ATAQUE RECE(-!)")
+    # print(df_exclamative)
+    # print("CONTRA ATAQUE")
+    # print(df_contra)
+    # print("-----------------")
 
 #file_paths,equipos1,equipos2 = info_test()
 print(leer_input("ruta_csv.txt"))
